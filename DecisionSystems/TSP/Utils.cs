319cc0245c68e09e191be33df6655c5caf82f74c
Pascal Gassner <pascal.gassner@htlvb.at>
2020-10-05 16:44:07 +0200

    Rewrite GetDistance using Ling

--- DecisionSystems/TSP/Utils.cs
+++ DecisionSystems/TSP/Utils.cs
@@ -8,18 +8,38 @@ namespace DecisionSystems.TSP
     {
         public static double GetDistance(IReadOnlyCollection<int> solution, IReadOnlyList<Location> cities)
         {
-            var distance = 0.0;
-            var previousCityIndex = solution.Last();
-            foreach (var cityIndex in solution)
+            //var distance = 0.0;
+            //var previousCityIndex = solution.Last();
+            //foreach (var cityIndex in solution)
+            //{
+            //    var previousCity = cities[previousCityIndex - 1];
+            //    var city = cities[cityIndex - 1];
+            //    distance += GetDistance(previousCity, city);
+            //    previousCityIndex = cityIndex;             
+            //}
+            //return distance;
+
+            int solutionIndex = 1;
+            var location = cities[solutionIndex - 1];
+
+            Location getLocation(int solutionIndex)
             {
-                var previousCity = cities[previousCityIndex - 1];
-                var city = cities[cityIndex - 1];
-                distance += GetDistance(previousCity, city);
-                previousCityIndex = cityIndex;
-                
+                return cities[solutionIndex - 1];
             }
-            return distance;
-         
+            return solution
+                .Append(solution.First())
+                .Select(getLocation)
+                .Pairwise(GetDistance)
+                .Sum();                 
+        }
+        public static IEnumerable<TOut> Pairwise<TIn, TOut> (this IEnumerable<TIn> items,Func<TIn, TIn, TOut> merge)
+        {
+            if (items is null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+
+            return items.Zip(items.Skip(1),merge);
         }
         public static double GetDistance(Location a, Location b)
         {
