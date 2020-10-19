94a3edac01db3ab4d258980efc4cfdc296fd47c4
Pascal Gassner <pascal.gassner@htlvb.at>
2020-10-19 16:44:52 +0200

    Rewrite GetDistance with Linq

---
+++ DecisionSystems/TSP/Solver/DummySolver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DecisionSystems.TSP.Solver
+{
+    public class DummySolver : ITSPSolver
+    {
+        public List<int> Solve(IReadOnlyList<Location> cities)
+        {
+            List<int> cityIndexes = new List<int>();
+
+            for (int i = 1; i < cities.Count+1; i++)
+            {
+                cityIndexes.Add(i);
+            }
+            return cityIndexes;
+        }
+    }
+}
